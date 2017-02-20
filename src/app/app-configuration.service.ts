6ea930ed1a5b6038751b4fad473ca8fb1ca1f117
Marcus Hellberg <marcus@vaadin.com>
2017-02-20 15:11:38 -0800

    paged loading of grid data

--- src/app/app-configuration.service.ts
+++ src/app/app-configuration.service.ts
@@ -2,5 +2,6 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class AppConfiguration {
-  public API_URL = `//${window.location.hostname}:8080`;
+  public API_URL = `https://comparison.demo.vaadin.com`;
+  // public API_URL = `//${window.location.hostname}:8080`;
 }
